b541eda9debf717c7a410096db417d9580b3abf8
DucksIncoming <ducksincoming@gmail.com>
2023-07-13 23:43:43 -0400

    Editing, saving, etc.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Course {
-    constructor(prefix, num, className, hrs, color, prereq, coreq){
+    constructor(prefix, num, className, hrs, color, prereq, coreq, category){
         this.prefix = prefix;
         this.num = num;
         this.className = className
@@ -7,51 +7,69 @@ class Course {
         this.prereq = prereq;
         this.coreq = coreq;
         this.color = color;
-        this.semester = -1
+        this.semester = -1;
+        this.category = category;
     }
 
-    isValid() {
-        this.semester = locateClass(this.prefix + " " + this.num);
-
-        if (this.semester == 0){
-            return true;
-        }
-        if (this.semester == -1){
-            return false;
-        }
-
-        for (let req = 0; req < this.prereq.length; req++){
-            let prereqLocation = locateClass(this.prereq[req]);
-            if (prereqLocation == -1){
-                return false;
-            }
-            else if (prereqLocation >= this.semester){
-                return false
-            }
-        }
-
-        for (let req = 0; req < this.coreq.length; req++){
-            let coreqLocation = locateClass(this.coreq[req]);
-            if (coreqLocation == -1){
-                return false;
-            }
-            else if (coreqLocation > this.semester){
-                return false
-            }
-        }
-
-        return true;
+    updateCourseInfo(prefix, num, className, hrs, color, prereq, coreq, category) {
+        this.prefix = prefix;
+        this.num = num;
+        this.className = className
+        this.hrs = hrs;
+        this.prereq = prereq;
+        this.coreq = coreq;
+        this.color = color;
+        this.semester = -1;
+        this.category = category;
     }
 }
 
+function isValid(course) {
+    course.semester = locateClass(course.prefix + " " + course.num);
+
+    if (course.semester == 0){
+        return true;
+    }
+    if (course.semester == -1){
+        return false;
+    }
+
+    for (let req = 0; req < course.prereq.length; req++){
+        let prereqLocation = locateClass(course.prereq[req]);
+        if (prereqLocation == -1){
+            return false;
+        }
+        else if (prereqLocation >= course.semester){
+            return false
+        }
+    }
+
+    for (let req = 0; req < course.coreq.length; req++){
+        let coreqLocation = locateClass(course.coreq[req]);
+        if (coreqLocation == -1){
+            return false;
+        }
+        else if (coreqLocation > course.semester){
+            return false
+        }
+    }
+
+    return true;
+}
+
+var saveData = JSON.parse(localStorage.saveData || null) || {};
+
+let courseData = retrieveSavedData();
+
+/*
 let a = new Course("MATH", "301", "Differential Equations", 3, "yellow", ["MATH 200"], [])
 let b = new Course("MATH", "200", "Calculus II", 3, "yellow", [], [])
 let c = new Course("MATH", "307", "Multivariate Calculus", 3, "yellow", ["MATH 200"], ["MATH 301"])
 
 let courseData = [
     [], //External Credit
-    [], //Semester 1
-    [b], //Semester 2
+    [a,b,c,a,b,c,a,b,c,a,b,c], //Semester 1
+    [], //Semester 2
     [], //Semester 3
     [], //Semester 4
     [], //Semester 5
@@ -59,10 +77,12 @@ let courseData = [
     [], //Semester 7
     []  //Semester 8
 ];
+*/
 
 let draggables = document.querySelectorAll(".class-box");
 let draggedClass;
-let semesterDivs = [
+let semesterDivs = [    
+                        document.getElementById("external-credit-container"),
                         document.getElementById("freshman-fall"),
                         document.getElementById("freshman-spring"),
                         document.getElementById("sophomore-fall"),
@@ -81,7 +101,16 @@ semesterDivs.forEach(container => {
         
         targetSemester = nameToSemester(container.id);
     });
-})
+});
+let externalCreditContainer = document.getElementById("external-credit-container");
+
+externalCreditContainer.addEventListener("dragover", e =>{
+    e.preventDefault();
+    let draggable = document.querySelector(".dragging");
+    externalCreditContainer.appendChild(draggable);
+    
+    targetSemester = 0;
+});
 
 let deleteAllDialogue = document.getElementById("delete-all-dialogue");
 let deleteAllConfirm = document.getElementById("delete-all-confirm");
@@ -93,17 +122,7 @@ deleteAllButton.addEventListener("click", e => {
 });
 deleteAllConfirm.addEventListener("click", e => {
     deleteAllDialogue.close();
-    courseData = courseData = [
-        [], //External Credit
-        [], //Semester 1
-        [], //Semester 2
-        [], //Semester 3
-        [], //Semester 4
-        [], //Semester 5
-        [], //Semester 6
-        [], //Semester 7
-        []  //Semester 8
-    ];
+    courseData = [[],[],[],[],[],[],[],[],[]];
     buildSchedule();
 });
 deleteAllCancel.addEventListener("click", e => {
@@ -111,20 +130,6 @@ deleteAllCancel.addEventListener("click", e => {
 });
 
 buildSchedule();
-
-document.getElementById("file-input").addEventListener("change", function (e) {
-    let file = document.getElementById("file-input").value;
-    let fileIndicator = document.getElementById("degreeworks-filename");
-    
-    filename = file.toString().split("\\")[2];
-
-    fileIndicator.textContent = filename;
-});
-
-function sendFileData() {
-    let fileIndicator = document.getElementById("degreeworks-filename");
-    fileIndicator.textContent = "No File Uploaded";
-}
 
 function locateClass(className){
     let prefix = className.split(" ")[0]
@@ -161,16 +166,50 @@ function nameToSemester(name){
             return 7;
         case "senior-spring":
             return 8;
+        default:
+            return 0;
     }
 }
 
+document.getElementById("edit-class-confirm").addEventListener("click", editCourse);
+
+function editCourse() {
+    let in_prefix = document.getElementById("edit-class-prefix").value;
+    let in_num = document.getElementById("edit-class-num").value;
+    let in_hours = document.getElementById("edit-class-hours").value;
+    let in_name = document.getElementById("edit-class-name").value;
+    let in_prerequisites = document.getElementById("edit-class-prerequisites").value;
+    let in_corequisites = document.getElementById("edit-class-corequisites").value;
+    let in_category = document.getElementById("edit-class-category").value;
+
+    let prereqArray = in_prerequisites.split(",");
+    let coreqArray = in_corequisites.split(",");
+
+    if (prereqArray[0] == ""){
+        prereqArray = []
+    }
+    if (coreqArray[0] == ""){
+        coreqArray = []
+    }
+
+    editingCourse.updateCourseInfo(in_prefix, in_num, in_name, parseInt(in_hours), classColors[parseInt(in_category)], prereqArray, coreqArray, in_category);
+    courseData[editSemester][editIndex] = editingCourse;
+
+    buildSchedule();
+    document.getElementById("edit-existing-class-dialogue").close()
+}
+
+var editingCourse;
+var editIndex;
+var editSemester;
+
 function buildSchedule() {
-    for (let sem = 1; sem < courseData.length; sem++){
-        semesterDivs[sem-1].innerHTML = "";
+    for (let sem = 0; sem < courseData.length; sem++){
+        semesterDivs[sem].innerHTML = "";
         let semesterHours = 0;
 
         for (let cl = 0; cl < courseData[sem].length; cl++){
-            let classBoxHtml = '<div class="class-box" draggable="true" style="background-color: [COLOR]; border: [BORDER]; z-index: [INDEX]"><input type="image" src="Icons/x-icon.png" class="class-box-delete"></input><b class="class-box-text">[PREFIX] [NUM]</b><p class="class-box-text">[CLASSNAME]</p><p class="class-box-text">[HOURS] hrs.</p></div>'
+            let classBoxHtml = '<div class="class-box" draggable="true" style="background-color: [COLOR]; border: [BORDER]; z-index: [INDEX]"><input type="image" src="Icons/x-icon.png" class="class-box-delete" title="Delete This Course"></input><input type="image" src="Icons/edit-icon.png" title="Edit This Course" class="class-box-edit"></input><b class="class-box-text">[PREFIX] [NUM]</b><p class="class-box-text">[CLASSNAME]</p><p class="class-box-text">[HOURS] hrs.</p></div>'
             let standardBorder = "0.15vw solid black";
             let errorBorder = "0.15vw solid red";
             let activeClass = courseData[sem][cl];
@@ -181,8 +220,8 @@ function buildSchedule() {
             classBoxHtml = classBoxHtml.replace("[HOURS]", activeClass.hrs);
             semesterHours += activeClass.hrs;
             classBoxHtml = classBoxHtml.replace("[INDEX]", (cl + 100).toString());
-            
-            if (activeClass.isValid()) {
+
+            if (isValid(activeClass)) {
                 classBoxHtml = classBoxHtml.replace("[BORDER]", standardBorder);
                 classBoxHtml = classBoxHtml.replace("[COLOR]", activeClass.color);
             }
@@ -191,10 +230,15 @@ function buildSchedule() {
                 classBoxHtml = classBoxHtml.replace("[COLOR]", "#ff6666");
             }
 
-            semesterDivs[sem-1].innerHTML += classBoxHtml;
+            semesterDivs[sem].innerHTML += classBoxHtml;
         }
 
-        semesterDivs[sem-1].innerHTML += '<p class="semester-hours">' + semesterHours.toString() + ' hrs.</p>'
+        if (sem != 0){
+            semesterDivs[sem].innerHTML += '<p class="semester-hours">' + semesterHours.toString() + ' hrs.</p>'
+        } 
+        else {
+            semesterDivs[sem].innerHTML += '<p class="external-credit-title">External Credit (AP, Dual Enrollment, etc.)</p>'
+        }
     }
 
     draggables = document.querySelectorAll(".class-box");
@@ -223,11 +267,15 @@ function buildSchedule() {
 
         draggable.addEventListener("mouseover", function (e) {
             let deleteButton = draggable.querySelector(".class-box-delete");
+            let editButton = draggable.querySelector(".class-box-edit");
             deleteButton.style.visibility = "visible";
+            editButton.style.visibility = "visible";
         });
         draggable.addEventListener("mouseout", function (e) {
             let deleteButton = draggable.querySelector(".class-box-delete");
+            let editButton = draggable.querySelector(".class-box-edit");
             deleteButton.style.visibility = "hidden";
+            editButton.style.visibility = "hidden";
         });
     });
     document.querySelectorAll(".class-box-delete").forEach(deleteB => {
@@ -242,11 +290,63 @@ function buildSchedule() {
             }
         });
     });
+    document.querySelectorAll(".class-box-edit").forEach(editB => {
+        editB.addEventListener("click", e => {
+            editIndex = (editB.parentElement.style.zIndex - 100);
+            editSemester = nameToSemester(editB.parentElement.parentElement.id);
+            editingCourse = courseData[editSemester][editIndex];
+
+            let prereqText = "";
+            editingCourse.prereq.forEach(req => {
+                prereqText += req + ",";
+            });
+            prereqText = prereqText.slice(0, -1);
+            let coreqText = "";
+            editingCourse.coreq.forEach(req => {
+                coreqText += req + ",";
+            });
+            coreqText = coreqText.slice(0, -1);
+            
+            document.getElementById("edit-existing-class-dialogue").showModal();
+            document.getElementById("edit-class-prefix").value = editingCourse.prefix;
+            document.getElementById("edit-class-num").value = editingCourse.num;
+            document.getElementById("edit-class-hours").value = editingCourse.hrs;
+            document.getElementById("edit-class-name").value = editingCourse.className;
+            document.getElementById("edit-class-prerequisites").value = prereqText;
+            document.getElementById("edit-class-corequisites").value = coreqText;
+            document.getElementById("edit-class-corequisites").value = coreqText;
+            document.getElementById("edit-class-category").selectedIndex = editingCourse.category;
+        });
+    });
 }
+
+function retrieveSavedData() {
+    return saveData.courseData || [[],[],[],[],[],[],[],[],[]];
+}
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 document.getElementById("new-class-cancel").addEventListener("click", e => {
     document.getElementById("add-new-class-dialogue").close()
 });
+document.getElementById("edit-class-cancel").addEventListener("click", e => {
+    document.getElementById("edit-existing-class-dialogue").close()
+});
+
+document.getElementById("save-data-button").addEventListener("click", e => {
+    animateCheckmark();
+
+    saveData.courseData = courseData;
+    saveData.time = new Date().getTime();
+    localStorage.saveData = JSON.stringify(saveData);
+});
+
+async function animateCheckmark() {
+    let saveConfirmation = document.getElementById("save-confirmation-image");
+    saveConfirmation.classList.add("checkanim");
+    await delay(1000)
+    saveConfirmation.classList.remove("checkanim");
+}
 
 document.getElementById("new-class-add").addEventListener("click", e => {
     let in_prefix = document.getElementById("new-class-prefix").value;
@@ -270,7 +370,7 @@ document.getElementById("new-class-add").addEventListener("click", e => {
         coreqArray = []
     }
 
-    let addedCourse = new Course(in_prefix, in_num, in_name, parseInt(in_hours), classColors[parseInt(in_category)], prereqArray, coreqArray);
+    let addedCourse = new Course(in_prefix, in_num, in_name, parseInt(in_hours), classColors[parseInt(in_category)], prereqArray, coreqArray, in_category);
     courseData[1].push(addedCourse)
     buildSchedule();
 
@@ -307,8 +407,6 @@ function updateClassPreview(){
     let in_num = document.getElementById("new-class-num");
     let in_hours = document.getElementById("new-class-hours");
     let in_name = document.getElementById("new-class-name");
-    let in_prerequisites = document.getElementById("new-class-prerequisites");
-    let in_corequisites = document.getElementById("new-class-corequisites");
     let in_category = document.getElementById("new-class-category");
 
     document.getElementById("new-class-preview").remove();
